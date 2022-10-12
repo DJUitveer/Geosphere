c8f001cf30390ac78178c49a9832a0b1fcbf6fee
Neil Prins <ST101116992@vcconnect.edu.za>
2022-10-12 22:33:52 +0200

    DEV_NP: Implemented some Logout Functionality

--- app/src/main/java/com/neil/geosphere/ProfileActivity.java
+++ app/src/main/java/com/neil/geosphere/ProfileActivity.java
@@ -2,20 +2,26 @@ package com.neil.geosphere;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
     //Declaring Components
     private Button edit, delete, achievements, history, logOut;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        mAuth = FirebaseAuth.getInstance();
         //Initializing Components
         edit = findViewById(R.id.btnEdit);
         delete = findViewById(R.id.btnDeleteProfile);
@@ -23,6 +29,12 @@ public class ProfileActivity extends AppCompatActivity {
         history = findViewById(R.id.btnHistory);
         logOut = findViewById(R.id.btnLogout);
 
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogOut();
+            }
+        });
     }
 
     public void EditProfile() {
@@ -45,5 +57,6 @@ public class ProfileActivity extends AppCompatActivity {
     public void LogOut() {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+        Toast.makeText(this, "Logout Successful", Toast.LENGTH_SHORT).show();
     }
 }
